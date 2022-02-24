0aca0899a9c41b7b3ab5c2ccd49a35ae1055a5a3
Danielasilvadev <91729164+Danielasilvadev@users.noreply.github.com>
2022-02-24 12:44:55 +0000

    inteiros

---
+++ OutroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OutroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TiposVarios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number =8;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(number);
+        }
+    }
+}
